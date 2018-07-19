b9097b914cf64a95f4cbcf28dfe417c381bad847
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-07-19 15:15:21 -0300

    [ADD][WIP] First attempt to send a SMS

--- models/totalvoice_odoo.py
+++ models/totalvoice_odoo.py
@@ -1,7 +1,41 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+
+from totalvoice.cliente import Cliente
+
+client = Cliente("49c31c417f21915f1ced29182c5dea56", 'api.totalvoice.com.br')
 
 class TotalVoiceBase(models.Model):
     _name = 'totalvoice.base'
+
+    # number_from = fields.Char(
+    #     string="Your Number",
+    #     required=True,
+    #     help="Provide your TotalVoice's number",
+    # )
+
+    number_to = fields.Char(
+        string="Contact Number",
+        required=True,
+        help="Provide your contact's number",
+    )
+
+    message = fields.Char(
+        string="Message",
+        required=True,
+        size=160,
+    )
+
+    response = fields.Text(
+        string="Sender,Receiver,Message",
+        readonly=True,
+    )
+
+    @api.multi
+    def send_sms(self):
+        for record in self:
+            response = client.sms.enviar(record.number_to, record.message)
+
+            record.response = response
 
