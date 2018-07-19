05d810b5a6e1ab31e8ae2a6f4eb1275b6561f9ff
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-07-18 21:09:39 -0300

    [ADD] Module Created

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "TotalVoice Odoo",
+    'summary': """ This application is intended to receive SMS and perform automatic actions from these messages. """,
+    'version': '10.0.1.0.0',
+    'category': 'SMS app',
+    'website': "https://www.kmee.com.br/",
+    'author': "KMEE Informática LTDA",
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'images': ['images/main_screenshot.png'],
+    'depends': ['base'],
+    'data': [
+         'views/totalvoice_base_view.xml',
+    ],
+}
---
+++ models/totalvoice_odoo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class TotalVoiceBase(models.Model):
+    _name = 'totalvoice.base'
+
